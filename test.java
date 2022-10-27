d12d3667a7eaa297cad4a4a7980e683b8745fc6d
Shoot <103382369+shoot649854@users.noreply.github.com>
2022-10-26 21:16:20 -0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,22 @@
+import java.io.*;  
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] arg)
+    {
+        String file = "/Users/shotomorisaki/Desktop/Pandemic Visualization/Data/owid-covid-data.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            while((line = br.readLine()) != null)
+            {
+                String
+            }
+        }
+    }
+}
